98800a367496add492eb3f5e85dba41f25dd606c
miiilyy <emili.silva@estudante.ifms.edu.br>
2023-10-17 08:28:29 -0400

    Novo

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -4,20 +4,23 @@ function contato(){
     return(
         <>
 <form action="contato">
-    <div>
+    <div className= "contato-form ">
+        <a>Formulário Cadastro</a>
+    <div className="contato-container">
         <label htmlFor="nome">Nome</label>
         <input type="texto" id="nome" name="nome_usuario"></input>
-    </div>
-    <div>
+    <div className= "contato-container">
         <label htmlFor="email">E_mail</label>
         <input type="email" id="email" name="email_usuario"></input>
     </div>
-    <div>
+    <div className="contato-container">
         <label htmlFor="texto">Texto:</label>
         <textarea id="texto" name="texto_usuario"></textarea>
     </div>    
     <div className="button">
  <button type="submit">Enviar</button>
+    </div>
+    </div>
     </div>
 </form>
         </>
