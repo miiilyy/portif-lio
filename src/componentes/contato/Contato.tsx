d9617cd8db284118dae2aaabe820d54c788a2ea6
miiilyy <emili.silva@estudante.ifms.edu.br>
2023-10-20 08:42:22 -0400

    novo

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,21 +1,39 @@
 import './contato.css'
 import '../header/Header'
+import React, { useState } from 'react'
 function contato(){
+        const [nome, setNome] = useState("")  
+        const [email, setEmail] = useState("")
+        const [mensagem, setMensagem] = useState("")
+        function mudaNome(e:React.ChangeEvent<HTMLInputElement>){
+          console.log(e.target.value)
+          setNome(e.target.value)
+        }
+        function mudaEmail(e:React.ChangeEvent<HTMLInputElement>){
+          console.log(e.target.value)
+          setEmail(e.target.value)
+        }
+        function mudaMensagem(e:React.ChangeEvent<HTMLTextAreaElement>){
+          console.log(e.target.value)
+          setMensagem(e.target.value)
+        }
+
+
     return(
         <>
 <form action="contato">
     <div className= "contato-form ">
         <a>Formulário Cadastro</a>
     <div className="contato-container">
-        <label htmlFor="nome">Nome</label>
-        <input type="texto" id="nome" name="nome_usuario"></input>
+        <label htmlFor="nome">Nome {nome}</label>
+        <input type="texto" id="nome" name="nome_usuario" onChange={mudaNome}></input>
     <div className= "contato-container">
-        <label htmlFor="email">E_mail</label>
-        <input type="email" id="email" name="email_usuario"></input>
+        <label htmlFor="email">E_mail{email}</label>
+        <input type="email" id="email" name="email_usuario" onChange={mudaEmail}></input>
     </div>
     <div className="contato-container">
-        <label htmlFor="texto">Texto:</label>
-        <textarea id="texto" name="texto_usuario"></textarea>
+        <label htmlFor="texto">Texto:{mensagem}</label>
+        <textarea id="texto" name="texto_usuario" onChange={mudaMensagem}></textarea>
     </div>    
     <div className="button">
  <button type="submit">Enviar</button>
